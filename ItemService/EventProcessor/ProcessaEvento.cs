b44d0a53c11aeb0e97f4df9a0090d907a786dba7
lucasMontagnani <lucascalilelias@yahoo.com.br>
2024-10-31 01:33:39 -0300

    Adicionar arquivos de projeto.

---
+++ ItemService/EventProcessor/ProcessaEvento.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using ItemService.Data;
+using ItemService.Dtos;
+using ItemService.Models;
+using System.Text.Json;
+
+namespace ItemService.EventProcessor
+{
+    public class ProcessaEvento : IProcessaEvento
+    {
+        private readonly IMapper _mapper;
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public ProcessaEvento(IMapper mapper, IServiceScopeFactory scopeFactory)
+        {
+            _mapper = mapper;
+            _scopeFactory = scopeFactory;
+        }
+
+        public void Processa(string mensagem)
+        {
+            /* Não será possível realizar a injeção do ItemRepository dentro do ProcessaEvento, pois este é uma dependência de
+             * RabbitMqSubscriber, o qual herda de BackgroundService, este que não é compativel com o tipo AddScoped do repository.
+             * Para conseguirmos fazer isso, precisaremos contornar o problema, fazendo a instância através do escopo factory.
+             * i.e:Instanciamos um contexto em que o BackgroundService vai conseguir fazer a injeção de dependências do ItemRepository
+             */
+            using var scope = _scopeFactory.CreateScope();
+
+            IItemRepository itemRepository = scope.ServiceProvider.GetRequiredService<IItemRepository>();
+
+            RestauranteReadDto? restauranteReadDto = JsonSerializer.Deserialize<RestauranteReadDto>(mensagem);
+
+            Restaurante restaurante = _mapper.Map<Restaurante>(restauranteReadDto);
+
+            if (!itemRepository.ExisteRestauranteExterno(restaurante.Id))
+            {
+                itemRepository.CreateRestaurante(restaurante);
+                itemRepository.SaveChanges();
+            }
+        }
+    }
+}
---
+++ ItemService/RabbitMqClient/RabbitMqSubscriber.cs
@@ -0,0 +1,48 @@
+﻿using ItemService.EventProcessor;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace ItemService.RabbitMqClient
+{
+    public class RabbitMqSubscriber : BackgroundService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _nomeDaFila;
+        private readonly IConnection _connection;
+        private IModel _channel;
+        private IProcessaEvento _processaEvento;
+
+        public RabbitMqSubscriber(IConfiguration configuration, IProcessaEvento processaEvento)
+        {
+            _configuration = configuration;
+            _connection = new ConnectionFactory() 
+                         { HostName = _configuration["RabbitMqHost"], 
+                           Port = Int32.Parse(_configuration["RabbitMqPort"]) 
+                         }.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
+            _nomeDaFila = _channel.QueueDeclare().QueueName;
+            _channel.QueueBind(queue: _nomeDaFila, exchange: "trigger", routingKey: "");
+            _processaEvento = processaEvento;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            EventingBasicConsumer? consumidor = new EventingBasicConsumer(_channel);
+            // O EventArgs é basicamente o que temos de conteúdo bruto dentro desse consumidor que estamos processando.
+            consumidor.Received += (ModuleHandle, ea) =>
+            {
+                ReadOnlyMemory<byte> body = ea.Body;
+                string? mensagem = Encoding.UTF8.GetString(body.ToArray());
+                _processaEvento.Processa(mensagem);
+            };
+
+            // Precisamos informar que consumimos o conteúdo dessa fila
+            // O autoAck indica que foi feito o processamento e envia essa informação, informando que foi consumido.
+            _channel.BasicConsume(queue: _nomeDaFila, autoAck: true, consumer: consumidor);
+
+            return Task.CompletedTask;
+        }
+    }
+}
